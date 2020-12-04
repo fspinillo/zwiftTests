02a1f9b5a572ecb5ea97c760cc6829f3997d23ed
Frank Spinillo <>
2020-12-04 06:50:09 -0500

    Adds in page load check

--- specs/homepage.spec.ts
+++ specs/homepage.spec.ts
@@ -3,6 +3,14 @@ import { home } from '../pages'
 describe('Homepage', () => {
     before('Load homepage', () => {
         browser.url('./')
+        browser.waitUntil(() => {
+            const state = browser.execute(() =>{
+                return document.readyState
+            })
+            return String(state) === 'complete'
+        }, {
+            timeoutMsg: 'Page was unable to load'
+        })
     })
 
     it('Verify Welcome Button', () => {
